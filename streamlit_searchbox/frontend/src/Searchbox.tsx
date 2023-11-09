1201da5af14448fe9c65fb6f928a629b75df35e6
magnesyljuasen <msylju@gmail.com>
2023-11-09 20:35:05 +0100

    kkk

---
+++ streamlit_searchbox/frontend/src/Searchbox.tsx
@@ -0,0 +1,127 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from "streamlit-component-lib";
+import React, { ReactNode } from "react";
+import Select from "react-select";
+
+import SearchboxStyle from "./styling";
+
+interface State {
+  menu: boolean;
+}
+
+interface StreamlitReturn {
+  interaction: "submit" | "search" | "reset";
+  value: any;
+}
+
+export function streamlitReturn(interaction: string, value: any): void {
+  Streamlit.setComponentValue({
+    interaction: interaction,
+    value: value,
+  } as StreamlitReturn);
+}
+
+class Searchbox extends StreamlitComponentBase<State> {
+  public state = { menu: false };
+
+  private style = new SearchboxStyle(this.props.theme!);
+  private ref: any = React.createRef();
+
+  /**
+   * new keystroke on searchbox
+   * @param input
+   * @param _
+   * @returns
+   */
+  private onSearchInput = (input: string, _: any): void => {
+    // happens on selection
+    if (input.length === 0) {
+      this.setState({ menu: false });
+      return;
+    }
+
+    streamlitReturn("search", input);
+  };
+
+  /**
+   * input was selected from dropdown or focus changed
+   * @param option
+   * @returns
+   */
+  private onInputSelection(option: any): void {
+    // clear selection (X)
+    if (option === null) {
+      this.callbackReset();
+      return;
+    }
+
+    this.callbackSubmit(option);
+  }
+
+  /**
+   * reset button was clicked
+   */
+  private callbackReset(): void {
+    this.setState({
+      menu: false,
+    });
+    streamlitReturn("reset", null);
+  }
+
+  /**
+   * submitted selection, clear optionally
+   * @param option
+   */
+  private callbackSubmit(option: any) {
+    streamlitReturn("submit", option.value);
+
+    if (this.props.args.clear_on_submit) {
+      this.ref.current.select.clearValue();
+    } else {
+      this.setState({
+        menu: false,
+      });
+    }
+  }
+
+  /**
+   * show searchbox with label on top
+   * @returns
+   */
+  public render = (): ReactNode => {
+    return (
+      <div>
+        {this.props.args.label ? (
+          <div style={this.style.label}>{this.props.args.label}</div>
+        ) : null}
+        <Select
+          inputId={this.props.args.label || "searchbox-input-id"}
+          // dereference on clear
+          ref={this.ref}
+          isClearable={true}
+          isSearchable={true}
+          styles={this.style.select}
+          options={this.props.args.options}
+          placeholder={this.props.args.placeholder}
+          // component overrides
+          components={{
+            ClearIndicator: (props) => this.style.clearIndicator(props),
+            DropdownIndicator: () => this.style.iconDropdown(this.state.menu),
+            IndicatorSeparator: () => <div></div>,
+          }}
+          // handlers
+          filterOption={(_, __) => true}
+          onChange={(e) => this.onInputSelection(e)}
+          onInputChange={(e, a) => this.onSearchInput(e, a)}
+          onMenuOpen={() => this.setState({ menu: true })}
+          onMenuClose={() => this.setState({ menu: false })}
+          menuIsOpen={this.props.args.options && this.state.menu}
+        />
+      </div>
+    );
+  };
+}
+export default withStreamlitConnection(Searchbox);
---
+++ streamlit_searchbox/frontend/src/styling.tsx
@@ -0,0 +1,133 @@
+import React from "react";
+import {
+  CSSObjectWithLabel,
+  StylesConfig,
+  ControlProps,
+  OptionProps,
+} from "react-select";
+
+import { ReactComponent as Dropdown } from "./icons/st-arrow.svg";
+import { ReactComponent as Clear } from "./icons/st-clear.svg";
+
+class SearchboxStyle {
+  theme: any;
+  label: any;
+  select: StylesConfig;
+
+  constructor(theme: any) {
+    this.theme = theme;
+    this.label = {
+      color: theme.textColor,
+      fontSize: "0.82em",
+      fontWeight: 400,
+      font: theme.font,
+      marginBottom: "0.85rem",
+    };
+
+    this.select = buildStyleSelect(theme);
+  }
+
+  /**
+   * Dropdown icon, adjusted if menu is open
+   * @param menu
+   * @returns
+   */
+  iconDropdown(menu: boolean) {
+    return (
+      <div>
+        <Dropdown
+          // streamlit has fixed icon sizes at 15x15
+          width={15}
+          height={15}
+          fill={this.theme.textColor}
+          style={{
+            marginRight: menu ? "7px" : "8px",
+            marginBottom: menu ? "0px" : "3px",
+          }}
+        />
+      </div>
+    );
+  }
+
+  /**
+   * Clear icon, props are passed by react-select
+   * @param props
+   * @returns
+   */
+  clearIndicator(props: any) {
+    const {
+      innerProps: { ref, ...restInnerProps },
+    } = props;
+
+    return (
+      <Clear
+        {...restInnerProps}
+        ref={ref}
+        fill={this.theme.fadedText60}
+        // streamlit has fixed icon sizes at 15x15
+        width={15}
+        height={15}
+      />
+    );
+  }
+}
+
+function buildStyleSelect(theme: any): any {
+  return {
+    // overall option list
+    menuList: (styles: any) => ({
+      ...styles,
+      backgroundColor: theme.backgroundColor,
+    }),
+    // filler text and icons
+    input: (styles: any) => ({ ...styles, color: theme.textColor }),
+    //
+    singleValue: (styles: any) => ({
+      ...styles,
+      color: theme.textColor,
+    }),
+    // placeholder text
+    placeholder: (styles: any) => ({
+      ...styles,
+      color: theme.fadedText60,
+    }),
+    // searchbox and others, e.g. options window
+    control: (styles: CSSObjectWithLabel, { isFocused }: ControlProps) => {
+      return {
+        ...styles,
+        backgroundColor: theme.secondaryBackgroundColor,
+        color: theme.secondaryBackgroundColor,
+        border: !isFocused
+          ? "1px transparent"
+          : "1px solid " + theme.primaryColor,
+        boxShadow: "none",
+        "&:hover": {
+          border: !isFocused
+            ? "1px transparent"
+            : "1px solid " + theme.primaryColor,
+        },
+      };
+    },
+    // single cell in option list
+    option: (
+      styles: CSSObjectWithLabel,
+      { isDisabled, isFocused, isSelected }: OptionProps
+    ) => {
+      return {
+        ...styles,
+        backgroundColor: isDisabled
+          ? undefined
+          : isSelected
+          ? theme.secondaryBackgroundColor
+          : isFocused
+          ? theme.secondaryBackgroundColor
+          : theme.backgroundColor,
+        // option text
+        color: theme.textColor,
+        cursor: isDisabled ? "not-allowed" : "Search ...",
+      };
+    },
+  };
+}
+
+export default SearchboxStyle;
